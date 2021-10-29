1a9e018e54cfeebc25b45a4e92626edf142575ff
C-SungminLee <91634957+C-SungminLee@users.noreply.github.com>
2021-10-29 13:58:19 -0700

    Add files via upload

--- Game2.py
+++ Game2.py
@@ -11,10 +11,23 @@ ufo = trtl.Turtle()
 
 blu = trtl.Turtle()
 
+
+ghost = trtl.Turtle()
+
+ghost.speed(1)
+
+
 wn.addshape('IMG_0933.gif')
+wn.addshape('ghost-flash.gif')
 blu.penup()
 blu.goto(0,-250)
 blu.shape('IMG_0933.gif')
+
+ghost.penup()
+ghost.hideturtle()
+ghost.shape('ghost-flash.gif')
+ghost.goto(blu.xcor(),blu.ycor())
+
 
 def left():
   x = blu.xcor()
@@ -26,7 +39,20 @@ def right():
   if -300<x<300: 
     blu.setheading(0)
     blu.forward(10)
+def attack():
+  ghost.clone()
+  ghost.hideturtle()
+  ghost.setheading(90) 
+  ghost.showturtle() 
+  ghost.forward(500)
 
+
+def space():
+  attack()
+
+
+  
+'''
 # Temporary 
 ufo.penup()
 ufo.goto(0, 300)
@@ -35,18 +61,20 @@ ufo.goto(0, 300)
 def ufomove():
   ufo.speed(5)
   lengthtomove = random.randint(-100,100)
-  ufo.goto(0+lengthtomove,300)
+  ufo.goto(+lengthtomove,250)
 
 while True:
   ufomove()
   time.sleep(1)
+'''
 # Snytax for keypress
 
 wn.onkeypress(left, 'Left')
 wn.onkeypress(right, 'Right')
+wn.onkeypress(space, 'Up')
 wn.listen()
 
 
 
 
-wn.mainloop()
+wn.mainloop() 
